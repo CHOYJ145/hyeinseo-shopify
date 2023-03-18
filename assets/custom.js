0e4ab35bf38f05468fdaf83cdfc39a916b990d60
CHOYJ145 <74890282+CHOYJ145@users.noreply.github.com>
2023-03-18 11:00:53 +0900

    main, login

--- assets/custom.js
+++ assets/custom.js
@@ -1,15 +1,26 @@
-function menuBtn(e){
-   document.querySelector('.header__inline-menu').classList.toggle('on');
-     document.querySelector('.header-util').classList.toggle('on');
-  if(document.querySelector('.header__inline-menu').classList.contains('on')){
-    document.querySelector('.menu-btn--mo span').innerText = 'Close';
-  }else{
-    document.querySelector('.menu-btn--mo span').innerText = 'Menu';
-  }
+function menuBtn(e) {
+    document.querySelector('.header__inline-menu').classList.toggle('on');
+    document.querySelector('.header-util').classList.toggle('on');
+    if (document.querySelector('.header__inline-menu').classList.contains('on')) {
+        document.querySelector('.menu-btn--mo span').innerText = 'Close';
+        // document.querySelector('header').classList.remove('header--white');
+    } else {
+        document.querySelector('.menu-btn--mo span').innerText = 'Menu';
+        // document.querySelector('header').classList.add('header--white');
+    }
 }
 
-function depthBtn(e){
-  document.querySelector('.header__2depth').classList.toggle('on');
-  document.querySelector('.header__inline-menu').classList.toggle('chk');
-  document.querySelector('.header-util').classList.toggle('chk');
+function depthBtn(e) {
+    if (e == 'shop') {
+        document.querySelector('.header__2depth').classList.add('on');
+        document.querySelector('.header__2depth--explore').classList.remove('on');
+        document.querySelector('.header__2depth--shop').classList.add('on');
+        // document.querySelector('.header__inline-menu').classList.add('chk');
+        // document.querySelector('.header-util').classList.add('chk');
+    } else if (e == 'explore') {
+        document.querySelector('.header__2depth').classList.add('on');
+        document.querySelector('.header__2depth--shop').classList.remove('on');
+        document.querySelector('.header__2depth--explore').classList.add('on');
+    }
+
 }
