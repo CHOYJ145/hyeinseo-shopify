5ff34dc72f716af0bd54869b4a4e37989947adf0
CHOYJ145 <74890282+CHOYJ145@users.noreply.github.com>
2023-03-22 08:10:24 +0900

    main / collection(blogs)

---
+++ assets/theme-blogs.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+    document.querySelector('.custom').classList.add('theme-black');
+    document.querySelector('.header').classList.remove('header--black');
+    document.querySelector('.header').classList.add('header--white');
+});
+
+$('.article-img').hover(function (){
+    let thisTitle = $(this).data('title');
+    $('.article-title').text(thisTitle);
+}, function(){
+    $('.article-title').text('');
+});
